64ebd534d2aa3eb860be574876877cf51635e17d
rishu srivastava <rishusrivastava0000@gmail.com>
2025-05-25 23:38:30 +0530

    update file

--- category.php
+++ category.php
@@ -8,6 +8,8 @@ if(!isset($_GET['id'])){
 $id = $_GET['id'];
  include "db_conn.php";
 
+include "user-header.php";
+
   include "php/func-book.php";
 $books = get_books_by_category($conn,$id);
 
@@ -31,40 +33,9 @@ $current_category = get_category($conn,$id);
 <link rel="stylesheet" href="css/style.css">   
 </head>
 <body>
+
     <div class="container">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Online Book Store</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Store</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li><li class="nav-item">
-            <?php if(isset($_SESSION['user_id'])){
-                
-                ?>
-                 
-                 <a class="nav-link" href="admin.php">Admin</a>
-                 <?php }else{ ?>
-          <a class="nav-link" href="login.php">Login</a>
-           <?php }?>
-        </li>
-      
-            
-      </ul>
-      
-    </div>
-  </div>
-</nav>
+    
 <h1 class="display-4 p-3 fs-3">
     <a href="index.php" class="nd">
         <img src="img/left-arrow.png" width="35">
